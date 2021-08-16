cbaf6261677d01ea5b631aa41502dcf31d668f9c
grigorii <grigoriial54@gmail.com>
2021-08-16 17:44:20 +0300

    Changed model's dictionary and tokenizer parameters. Result improved. Wrote comments.

--- predict.py
+++ predict.py
@@ -1,17 +1,24 @@
+# Набор используемых библиотек все тот же.
+
 import pandas as pd
 from catboost import CatBoostClassifier, Pool
 
+# Аналогично загружаем модель из файла.
 
 model = CatBoostClassifier()
 model.load_model('model.cbm')
 
+# Аналогично читаем тестовые данные, создаем Pool.
+# Однако теперь в нем не будет целевой переменной - ее мы предсказываем.
 
 test = pd.read_csv('test.tsv', sep='\t')
 test_data = Pool(test['libs'], text_features=[0])
 
+# Наконец, делаем предсказание на тестовых данных.
 
 final_prediction = model.predict(test_data)
 
+# Записываем результаты в файл.
 
 with open('prediction.txt', 'w') as f:
     print('prediction', file=f)
--- validate.py
+++ validate.py
@@ -1,25 +1,33 @@
+# Библиотеку scikit-learn будем использовать для подсчета требуемых метрик.
+# Остальные библиотеки используются как и раньше.
+
 import pandas as pd
 from catboost import CatBoostClassifier, Pool
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
+# Читаем модель из ранее созданного файла.
 
 model = CatBoostClassifier()
 model.load_model('model.cbm')
 
+# Загружаем данные для проверки метрик модели сначала в DataFrame, а затем сразу в Pool.
+# Признак, целевая переменная и индикатор текстового признака остаются теми же.
 
 valid = pd.read_csv('val.tsv', sep='\t')
 valid_data = Pool(valid['libs'], valid['is_virus'], text_features=[0])
 
+# Получем предсказание модели на новых валидационных данных.
 
 valid_prediction = model.predict(valid_data)
 
+# Теперь пишем в файл значения, которые выдает наша модель на требуемых метриках.
 
 with open('validation.txt', 'w') as f:
-    m = confusion_matrix(valid['is_virus'], valid_prediction)
-    print("True positive: ", m[0][0], file=f)
-    print("False positive", m[0][1], file=f)
-    print("False negative", m[1][0], file=f)
-    print("True negative", m[1][1], file=f)
+    conf_matrix = confusion_matrix(valid['is_virus'], valid_prediction)
+    print("True positive: ", conf_matrix[0][0], file=f)
+    print("False positive", conf_matrix[0][1], file=f)
+    print("False negative", conf_matrix[1][0], file=f)
+    print("True negative", conf_matrix[1][1], file=f)
     print("Accuracy: ", accuracy_score(valid.is_virus, valid_prediction), file=f)
     print("Precision: ", precision_score(valid.is_virus, valid_prediction), file=f)
     print("Recall: ", recall_score(valid.is_virus, valid_prediction), file=f)
